6c481ccc05215c2878ebeb6250ccc09d7917b72e
Domenico Gemoli <domenicogemoli@gmail.com>
2019-04-14 12:59:24 +0200

    Change input mode from keyboard to mouse

--- Player.gd
+++ Player.gd
@@ -4,42 +4,44 @@ signal hit
 
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window
+var target = Vector2()
 
 func _ready():
     screen_size = get_viewport_rect().size
     hide()
 	
 func start(pos):
-    position = pos
-    show()
-    $CollisionShape2D.disabled = false
+	print("start")
+	position = pos
+	target = pos
+	show()
+	$CollisionShape2D.disabled = false
+	
+# Change the target whenever a touch event happens.
+func _input(event):
+	# print("input " + event.pressed)
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
 
 func _process(delta):
-    var velocity = Vector2()  # The player's movement vector.
-    if Input.is_action_pressed("ui_right"):
-        velocity.x += 1
-    if Input.is_action_pressed("ui_left"):
-        velocity.x -= 1
-    if Input.is_action_pressed("ui_down"):
-        velocity.y += 1
-    if Input.is_action_pressed("ui_up"):
-        velocity.y -= 1
-    if velocity.length() > 0:
+	var velocity = Vector2()
+	if position.distance_to(target) > 10:
+        velocity = (target - position).normalized() * speed
+	
+	if velocity.length() > 0:
         velocity = velocity.normalized() * speed
         $AnimatedSprite.play()
-   	else:
+	else:
         $AnimatedSprite.stop()
 
-    position += velocity * delta
-    position.x = clamp(position.x, 0, screen_size.x)
-    position.y = clamp(position.y, 0, screen_size.y)
+	position += velocity * delta
 
-    if velocity.x != 0:
+	if velocity.x != 0:
 	    $AnimatedSprite.animation = "right"
 	    $AnimatedSprite.flip_v = false
 	    # See the note below about boolean assignment
 	    $AnimatedSprite.flip_h = velocity.x < 0
-    elif velocity.y != 0:
+	elif velocity.y != 0:
 	    $AnimatedSprite.animation = "up"
 	    $AnimatedSprite.flip_h = false
 	    $AnimatedSprite.flip_v = velocity.y > 0
